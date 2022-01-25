e1936fd027725cc3241fa5128d97cb784c656d73
Eric Zhong <ezhong1900@gmail.com>
2022-01-24 22:15:37 -0500

    Add eric changess

--- gpt3.js
+++ gpt3.js
@@ -1,38 +1,59 @@
+// EDIT: moved these to top of the page
+const { request, response } = require('express'); // you don't need this, express is for server side apps
+const got = require('got');
+
 //listen for forwarded message from content (arriving thru background)
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.id === "forwardingToGPT3") {
     console.log(request.data)
     sendResponse({final:"gpt3 received message from content"})
+    // You should call getCompletion here if you want to get the completion based on the request.data
+    // like this:
+    getCompletion(request.data).then((resolve, reject) => {
+        // resolve is an array with the first element being a boolean indicating success or failure
+        // and the second element being the completion returned by OpenAI api
+        if (resolve[0]) {
+            sendResponse({ final: resolve[1] });
+        } else {
+            sendResponse({ final: "Error" });
+        }
+    })
+    // I'm not sure if you want to get the completion value here, or how you are sending the final
+    // back to background.js, but this should correctly call the api and handle the response.
+    // I can't test bc I don't have an api key.
   }
 })
 
-const { request, response } = require('express');
-const got = require('got');
-//prompt = selectedText
-const prompt = request.data;
+// EDIT: request from 'express' is different than the request object in 
+// the chrome.runtime.onMessage listener(function(request, ...)) function.
+// const prompt = request.data; // REMOVE THIS
 
-(async () => {
-  const url = 'https://api.openai.com/v1/engines/davinci/completions';
-  const params = {
-    "prompt": prompt,
-    "max_tokens": 30,
-    "temperature": 0.7,
-    "frequency_penalty": 0.5
-  };
-  const headers = {
-    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-  };
+// EDIT: before, you had an anonymous async function that executed automatically 
+// when the file was loaded. I created getCompletion, an async function that
+// you can call when yoou need to get a completion for the prompt.
+async function getCompletion(prompt) {
+    const url = 'https://api.openai.com/v1/engines/davinci/completions';
+    const params = {
+      "prompt": prompt,
+      "max_tokens": 30,
+      "temperature": 0.7,
+      "frequency_penalty": 0.5
+    };
+    const headers = {
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+    };
+  
+    try {
+      const response = await got.post(url, { json: params, headers: headers }).json();
+      output = `${prompt}${response.choices[0].text}`;
+      console.log(output);
+      return [true, output];
+    } catch (err) {
+      console.log(err);
+      return [false, null];
+    }
+}
 
-  try {
-    const response = await got.post(url, { json: params, headers: headers }).json();
-    output = `${prompt}${response.choices[0].text}`;
-    console.log(output);
-  } catch (err) {
-    console.log(err);
-  }
-})();
-
-console.log(output)
 //test sending dynamic text to popup.html
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.id === "requesting simplified text") {
